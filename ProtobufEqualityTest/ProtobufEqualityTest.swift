427d8595177fb3d4cb3ef5b3ecc3921f0d29ef8e
Mattijs Fuijkschot <mattijs@innovactory.com>
2015-05-01 13:03:44 +0200

    Initial commit

---
+++ ProtobufEqualityTest/ProtobufEqualityTest.swift
@@ -0,0 +1,54 @@
+//
+//  ProtobufEqualityTest.swift
+//  ProtobufEqualityTest
+//
+//  Created by Mattijs on 01/05/15.
+//
+//
+
+import XCTest
+
+class RegularPoint: Equatable {
+    var latitude:Float = Float(0)
+    var longitude:Float = Float(0)
+}
+
+
+func == (lhs: RegularPoint, rhs: RegularPoint) -> Bool {
+    return lhs.latitude == rhs.latitude &&
+        lhs.longitude == rhs.longitude
+}
+
+class ProtobufEqualityTest: XCTestCase {
+    
+    func testRegularPoint() {
+        var point1 = RegularPoint()
+        point1.latitude = 1.0
+        point1.longitude = 1.0
+        
+        var point2 = RegularPoint()
+        point2.latitude = 2.0
+        point2.longitude = 2.0
+        
+        // works fine, calls the == function and takes the inverse implicitly (put break point to check)
+        XCTAssert(point1 != point2, "")
+    }
+    
+    func testProtoPoint() {
+        var point1 = ProtoPoint.builder().setLatitude(1.0).setLongitude(1.0).build()
+        var point2 = ProtoPoint.builder().setLatitude(2.0).setLongitude(2.0).build()
+        
+        XCTAssert(point1.latitude == 1.0, "")
+        XCTAssert(point2.latitude == 2.0, "")
+        
+        // Fails, should call the == function from Point.pb.swift and take the inverse. But the function == is never invoked (put breakpoint on the == func to check)
+        XCTAssert(point1 != point2, "")
+    }
+}
+
+
+// implementing != below explicitly makes it works, but should not be needed
+//
+//internal func != (lhs: ProtoPoint, rhs: ProtoPoint) -> Bool {
+//    return !(lhs == rhs)
+//}
